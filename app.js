a9cf72cb17c6bd3098a1ea2b9885f858042cac09
Denys Severin <severindenys367@gmail.com>
2022-04-10 13:23:12 +0300

    setting up, adding items, duedate

---
+++ app.js
@@ -0,0 +1,43 @@
+"use strict";
+const todo = [
+  {
+    title: "Get groceries",
+    dueDate: "2022-05-11",
+  },
+  {
+    title: "Wash car",
+    dueDate: "2022-07-01",
+  },
+  {
+    title: "Get groceries",
+    dueDate: "2022-04-21",
+  },
+];
+
+render();
+
+function addTodo() {
+  const textBox = document.getElementById("todo-title"); // tells to get an html element by Id
+  const title = textBox.value; // to get the data we've typed out of textBox
+
+  const datePicker = document.getElementById("date-picker");
+  const dueDate = datePicker.value;
+  todo.push({
+    title: title,
+    dueDate: dueDate,
+  });
+
+  render();
+}
+
+function render() {
+  //reset our list
+  document.getElementById("todo-list").innerHTML = ""; //it will wipe out the list and replace it with empty string
+
+  todo.forEach(function (todo) {
+    const element = document.createElement("div"); // creates html element
+    element.innerText = todo.title + "" + todo.dueDate; //it takes our div and it sets the text inside the div to be the value of todo1
+    const todoList = document.getElementById("todo-list");
+    todoList.appendChild(element); // adds an element to the end of the body
+  });
+}
